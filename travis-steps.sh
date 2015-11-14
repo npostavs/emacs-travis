b84a89f437f10246a9e122d79da7ee896ae49f17
Noam Postavsky <npostavs@users.sourceforge.net>
2015-11-14 00:19:06 -0500

    add .travis.yml to time every make step

---
+++ travis-steps.sh
@@ -0,0 +1,32 @@
+# source me!
+
+if [ $EMACS_VERSION = 24.5 ] ; then
+download() {
+    curl -o "/tmp/emacs-${EMACS_VERSION}.tar.gz" "https://ftp.gnu.org/gnu/emacs/emacs-${EMACS_VERSION}.tar.gz"
+}
+unpack() {
+    tar xzf "/tmp/emacs-${EMACS_VERSION}.tar.gz" -C /tmp
+    mv /tmp/emacs-${EMACS_VERSION} /tmp/emacs
+}
+autogen() { :; }
+else
+download() {
+    git clone --depth=1 'http://git.sv.gnu.org/r/emacs.git' /tmp/emacs
+}
+unpack() { :; }
+autogen() {
+    cd /tmp/emacs && ./autogen.sh
+}
+fi
+
+configure() {
+    cd '/tmp/emacs' && ./configure --quiet --enable-silent-rules --prefix="${HOME}" ${EMACSCONFFLAGS}
+}
+
+do_make() {
+    make -j2 -C '/tmp/emacs' V=0 "$@"
+}
+
+# show definitions for log
+declare -f download unpack autogen configure do_make
+
